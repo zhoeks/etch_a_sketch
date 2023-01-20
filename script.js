cccd2016edb92ad808bf2437f47317e01282bce5
zhoeks <zhoeks@yahoo.com>
2023-01-20 11:06:07 -0600

    Created button for page and hover boxes in 16x16 grid

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 //create container div
 const mainBody = document.body;
 const div = document.createElement('div');
+const button = document.createElement('button');
+button.classList.add('restart');
+button.textContent = 'Start Over'
 div.classList.add('container');
+mainBody.appendChild(button);
 mainBody.appendChild(div);
 
+
 const createRow = (num) => {
-    for (let i = 0; i <= num; i++) {
+    for (let i = 0; i < num; i++) {
         const container = document.querySelector('.container');
         const create = document.createElement('div');
         create.classList.add('row'+[i]);
-        create.textContent = `Column ${i}`;
+        create.classList.add('gridBox');
+        create.textContent = 0;
         container.appendChild(create);
     }
 }
 
 const fillPage = (num) => {
-    for (let i = 0; i <= num; i++) {
-        createRow(num);
-        
+    for (let i = 0; i < num; i++) {
+        createRow(num);        
     }
 }
 
-fillPage(16);
+fillPage(25);
+
+const gridHover = document.querySelectorAll('.gridBox');
+gridHover.forEach((gridBox) => {
+    gridBox.addEventListener('mouseover', (e) => {
+        gridBox.classList.add('highlight');
+});
+});
+
